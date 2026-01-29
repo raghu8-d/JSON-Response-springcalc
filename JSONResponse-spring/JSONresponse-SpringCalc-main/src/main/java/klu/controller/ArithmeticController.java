5e18801caaae081543d2759bbcbb0701e395abe6
Raghuveer Narisetty <narisettyraghuveer827@gmail.com>
2026-01-29 08:07:31 +0530

    Add files via upload

---
+++ JSONResponse-spring/JSONresponse-SpringCalc-main/src/main/java/klu/controller/ArithmeticController.java
@@ -0,0 +1,23 @@
+package klu.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin("*")
+public class ArithmeticController {
+	@GetMapping("/add/{A}/{B}")
+	public String add(@PathVariable("A") int a,@PathVariable("B") int b)
+	{
+		return "Addition = " + (a+b);
+	}
+	@GetMapping("/mul/{A}/{B}")
+	public String add(@PathVariable("A") int a,@PathVariable("B") int b)
+	{
+		return "Multiplication = " + (a*b);
+	}
+}
